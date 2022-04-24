1504e616652567d214efc1df60aa24a6afddb90e
JonasFiechter <jonasrfiechter@gmail.com>
2022-04-24 15:59:36 -0300

    update
    searchbox now works.

--- robo_friends/src/App.js
+++ robo_friends/src/App.js
@@ -1,16 +1,34 @@
-import react from 'react';
+import React, {Component} from 'react';
 import CardList from './card_list';
 import SearchBox from './search_box'
 import { robots } from './robots.js'
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>Robo Friends</h1>
-            <SearchBox/>
-            <CardList robots={robots}/>
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            search_field: ''
+        }
+    }
+
+    on_search_change = (event) => {
+        this.setState({search_field: event.target.value})
+
+    }
+
+    render() {
+        const filtered_robots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.search_field.toLowerCase());
+        })
+        return (
+            <div className='tc'>
+                <h1>Robo Friends</h1>
+                <SearchBox search_change={this.on_search_change}/>
+                <CardList robots={filtered_robots}/>
+            </div>
+        );
+    };
 };
 
 export default App;
--- robo_friends/src/search_box.js
+++ robo_friends/src/search_box.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const SearchBox = () => {
+const SearchBox = ({search_field, search_change}) => {
     return (
         <div className="pa2">
             <input 
                 className="pa3 ba b--green bg-lightest-blue" 
                 type='search' 
                 placeholder="search bots"
+                onChange={search_change}
             />
         </div>
     )
